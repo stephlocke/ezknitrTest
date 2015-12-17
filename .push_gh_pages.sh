7e9f51068ba74f117d43269ae568ea891d676d3e
Steph Locke <slocke@mango-solutions.com>
2015-12-17 16:31:44 +0000

    Travis CI stuff

---
+++ .push_gh_pages.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com/stephlocke/Rtraining.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+R CMD BATCH '../ezknitrTest/rendercheck.R'
+
+cp ghgenerate.Rout out
+cd out
+git init
+git config user.name "stephs-travis"
+git config user.email "travis"
+git add .
+git commit -m "deployed to github pages"
+git push --force --quiet $FULL_REPO master:gh-pages
--- rendercheck.R
+++ rendercheck.R
@@ -1,8 +1,11 @@
+library(ezknitr)
 orig <- getwd()
+setwd("../ezknitrTest")
 
 # wd() to part level e.g. inst/lvl1a/
-ezknitr::ezknit("part1.Rmd","inst/lvl1a")
+ezknitr::ezknit("part1.Rmd","inst/lvl1a", keep_md = FALSE)
 
 # setwd() to top level e.g. inst/
-ezknitr::ezknit("part0.Rmd","inst")
+ezknitr::ezknit("part0.Rmd","inst", keep_md = FALSE)
 
+setwd(orig)
